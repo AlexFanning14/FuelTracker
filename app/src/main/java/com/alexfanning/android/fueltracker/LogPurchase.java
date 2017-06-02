d505d778abdd7618db8a4651a95af8c1c312c4cf
alex.fanning <alexfanning14@gmail.com>
2017-06-02 13:07:32 +0100

    Updates

--- app/src/main/java/com/alexfanning/android/fueltracker/LogPurchase.java
+++ app/src/main/java/com/alexfanning/android/fueltracker/LogPurchase.java
@@ -1,13 +1,58 @@
 package com.alexfanning.android.fueltracker;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.format.DateFormat;
+import android.text.format.DateUtils;
+import android.view.Menu;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class LogPurchase extends AppCompatActivity {
+
+    private TextView mTxtViewDate;
+    private EditText mEdtTxtLitres;
+    private EditText mEdtTxtPricePL;
+    private EditText mEdtTxtTotlPrice;
+    private EditText mEdtTxtGarage;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_purchase);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        findViews();
+
+        setDate();
+
+    }
+
+    private void setDate(){
+        Calendar c = Calendar.getInstance();
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MMM-yyyy");
+        mTxtViewDate.setText(sdf.format(c.getTime()));
+
+    }
+
+
+    private void findViews(){
+        mTxtViewDate = (TextView) findViewById(R.id.tvdateval);
+        mEdtTxtLitres = (EditText) findViewById(R.id.tbLitres);
+        mEdtTxtPricePL = (EditText) findViewById(R.id.tbPricePL);
+        mEdtTxtTotlPrice = (EditText) findViewById(R.id.tbTotPrice);
+        mEdtTxtGarage = (EditText) findViewById(R.id.tbGarageVal);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main,menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
--- app/src/main/java/com/alexfanning/android/fueltracker/MainActivity.java
+++ app/src/main/java/com/alexfanning/android/fueltracker/MainActivity.java
@@ -3,29 +3,43 @@ package com.alexfanning.android.fueltracker;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
 
-    Button btnLogPurchase;
-    Button btnRcntTransct;
-    Button btnStats;
-    Button btnNearbyGarage;
-
+    private Button btnLogPurchase;
+    private Button btnRcntTransct;
+    private Button btnStats;
+    private Button btnNearbyGarage;
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViews();
 
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    private void findViews(){
+        btnLogPurchase = (Button) findViewById(R.id.btnLogPurchaseMainMen);
+        btnRcntTransct = (Button) findViewById(R.id.btnRecent);
+        btnStats = (Button) findViewById(R.id.btnStats);
+        btnNearbyGarage= (Button) findViewById(R.id.btnViewGarage);
 
     }
 
-    public void btnLogPurchase_Clckd(View view){
-        Intent i = new Intent(getApplicationContext(),LogPurchase.class);
-        startActivity(i);
+    protected void btnLogTransaction_Clicked(View v){
+        startActivity(new Intent(this,LogPurchase.class));
     }
 
 
